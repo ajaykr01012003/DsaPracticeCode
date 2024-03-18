dd7439df2405ef2d332bb90fb606963d907d8a26
ajaykr01012003 <ajay.kr.01012003@gmail.com>
2024-03-18 17:19:20 +0530

    DSA

--- GenralProgramingArithmetic/PrimeFactrization.java
+++ GenralProgramingArithmetic/PrimeFactrization.java
@@ -4,7 +4,7 @@ public class PrimeFactrization {
     public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        for(int div=2; div*div<=n; div++){
+         for(int div=2; div*div<=n; div++){
             while(n%div ==0){
                 n=n/div;
                 System.out.print(div+" ");
